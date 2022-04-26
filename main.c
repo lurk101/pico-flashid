1634c768932679e547057c9473a4496df97008cb
lurk101 <lurkone0one@gmail.com>
2022-04-26 16:15:17 -0400

    optimize

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#include "stdinit.h"
+
 #if PICO_NO_FLASH
 #define FLASH_RUID_CMD 0x4b
 #define FLASH_RUID_DUMMY_BYTES 4
@@ -26,36 +28,31 @@ static void flash_cs_force(bool high) {
                     IO_QSPI_GPIO_QSPI_SS_CTRL_OUTOVER_BITS);
 }
 
-static void flash_do_cmd(const uint8_t* txbuf, uint8_t* rxbuf, size_t count) {
+static void flash_get_unique_id(uint8_t* id) {
     ((rom_flash_exit_xip_fn)rom_func_lookup(ROM_FUNC_FLASH_EXIT_XIP))();
     flash_cs_force(0);
-    uint32_t tx_remaining = count, rx_remaining = count;
+    uint8_t buf[FLASH_RUID_TOTAL_BYTES] = {FLASH_RUID_CMD};
+    uint8_t* txbuf = buf;
+    uint32_t tx_remaining = FLASH_RUID_TOTAL_BYTES, rx_remaining = FLASH_RUID_TOTAL_BYTES;
     while (tx_remaining || rx_remaining) {
         if ((ssi_hw->sr & SSI_SR_TFNF_BITS) && tx_remaining) {
             ssi_hw->dr0 = *txbuf++;
             --tx_remaining;
         }
         if ((ssi_hw->sr & SSI_SR_RFNE_BITS) && rx_remaining) {
-            *rxbuf++ = ssi_hw->dr0;
-            --rx_remaining;
+            uint8_t c = ssi_hw->dr0;
+            if (rx_remaining-- <= FLASH_RUID_DATA_BYTES)
+                *id++ = c;
         }
     }
     flash_cs_force(1);
     ((rom_flash_flush_cache_fn)rom_func_lookup(ROM_FUNC_FLASH_FLUSH_CACHE))();
     ((rom_flash_enter_cmd_xip_fn)rom_func_lookup(ROM_FUNC_FLASH_ENTER_CMD_XIP))();
 }
-
-static void flash_get_unique_id(uint8_t* id_out) {
-    uint8_t buf[FLASH_RUID_TOTAL_BYTES * 2] = {0};
-    buf[0] = FLASH_RUID_CMD;
-    flash_do_cmd(buf, buf + FLASH_RUID_TOTAL_BYTES, FLASH_RUID_TOTAL_BYTES);
-    __builtin_memcpy(id_out, buf + 1 + FLASH_RUID_DUMMY_BYTES + FLASH_RUID_TOTAL_BYTES,
-                     FLASH_RUID_DATA_BYTES);
-}
 #endif
 
 int main(void) {
-    stdio_init_all();
+    stdio_init();
     uint64_t id;
     flash_get_unique_id((uint8_t*)&id);
     printf("ID %llx\n", __builtin_bswap64(id));
